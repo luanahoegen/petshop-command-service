0b2ce37868f5c0f5fe1e47762f7251247429a7fe
Luana Hoegen <luanahoegen123@gmail.com>
2026-04-22 09:41:11 -0300

    cancelar-agenda

--- src/main/java/com/udesc/account/framework/AgendaResource.java
+++ src/main/java/com/udesc/account/framework/AgendaResource.java
@@ -2,11 +2,9 @@ package com.udesc.account.framework;
 
 import com.udesc.account.common.dto.AgendaDTO;
 import com.udesc.account.common.dto.CreateAgendaRequest;
+import com.udesc.account.common.service.CancelAgenda;
 import com.udesc.account.common.service.CreateAgenda;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/agenda")
@@ -15,13 +13,21 @@ import jakarta.ws.rs.core.MediaType;
 public class AgendaResource {
 
     private final CreateAgenda createAgenda;
+    private final CancelAgenda cancelAgenda;
 
-    public AgendaResource(CreateAgenda createAgenda) {
+    public AgendaResource(CreateAgenda createAgenda, CancelAgenda cancelAgenda) {
         this.createAgenda = createAgenda;
+        this.cancelAgenda = cancelAgenda;
     }
 
     @POST
     public AgendaDTO agendar(CreateAgendaRequest request) {
         return createAgenda.agendar(request);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public String cancelar(@PathParam("id") Long id) {
+        return cancelAgenda.cancelar(id);
     }
 }
